67d8ed21345ef61ae63d3077a9e6797ae92066d5
pjx206 <pjx206@163.com>
2020-08-01 10:40:09 +0800

    Now can copy contents in Sections view

--- SectionsView.cpp
+++ SectionsView.cpp
@@ -5,6 +5,12 @@ SectionsView::SectionsView(PEImage* peImage, QWidget* parent) {
 	peImageLocal = peImage;
 	ui.setupUi(this);
 	setupTableView();
+
+	// setup right click menu
+	menu = new QMenu(ui.SectionsTable);
+	action_copy = new QAction("Copy", ui.SectionsTable);
+	connect(action_copy, SIGNAL(triggered()), this, SLOT(copy_item()));
+	//connect(ui.SectionsTable, SIGNAL(itemClicked(QTableWidgetItem)), this, SLOT(click_debug()));
 	// SectionsTable是tableVertLayout的child，所以这样可以使得table填充满整个窗口
 	this->setLayout(ui.tableVertLayout);
 	ui.SectionsTable->horizontalHeader()->setStretchLastSection(true);
@@ -23,19 +29,12 @@ void SectionsView::setupTableView() {
 	headerText << "Name" << "V.Address" << "V.Size" << "V.RawSize" << "V.R.DeltaSize" << "Characteristics";
 	int count = headerText.count();
 	table->setColumnCount(count);
-	for (int i = 0; i < table->columnCount(); i++) {
-		headerItem = new QTableWidgetItem(headerText.at(i));
-		QFont font = headerItem->font();
-		font.setBold(true);
-		font.setPointSize(9);
-		headerItem->setFont(font);
-		table->setHorizontalHeaderItem(i, headerItem);
-	}
-
 
 	DWORD numSections = peImageLocal->sectionCount;
-	char namebuffer[9] = { 0 };
 	table->setRowCount(numSections);
+
+	table->setHorizontalHeaderLabels(headerText);
+	char namebuffer[9] = { 0 };
 	for (int i = 0; i < numSections; i++) {
 		PIMAGE_SECTION_HEADER secHeader = peImageLocal->SectionHeadersArr[i];
 		memset(namebuffer, 0, 8);
@@ -43,14 +42,51 @@ void SectionsView::setupTableView() {
 		QString name(namebuffer);
 		DWORD virtualAddr = secHeader->VirtualAddress;
 		DWORD virtualSize = secHeader->Misc.VirtualSize;
-		DWORD rawSize = secHeader->Misc.PhysicalAddress;
+		DWORD rawSize = secHeader->SizeOfRawData;
 		int sizeDelta = virtualAddr - rawSize;
 		DWORD characteristics = secHeader->Characteristics;
 		AddItemsOneRow(i, name, virtualAddr, virtualSize, rawSize, sizeDelta, characteristics);
 	}
-
-
+	// Debug
+	qDebug() << ui.SectionsTable->item(0, 0)->text();
 }
+
+//void SectionsView::show_menu(const QPoint pos) {
+//	
+//}
+void SectionsView::contextMenuEvent(QContextMenuEvent* event) {
+	menu->clear();
+	qDebug() << ui.SectionsTable->columnWidth(0);
+	QPoint point = event->pos();
+	qDebug() << '(' << point.x() << ',' << point.y() << ')';
+	if (!ui.SectionsTable->underMouse() || point.x() <= 25) {
+		// 25是左边行标的宽度，还没找到获取第0列的起始x坐标的方法，所以用这个先代替了。
+		event->ignore();
+		return;
+	}
+	selectedItem = ui.SectionsTable->currentItem(); // 设置选中的selectedItem
+	qDebug() << point.x() << point.y();
+	qDebug() << "row" << selectedItem->row() << ' ' << "col" << selectedItem->column() << selectedItem << selectedItem->text();
+	if (selectedItem == NULL) {
+		event->ignore();
+		return;
+	}
+	menu->addAction(action_copy);
+	menu->exec(QCursor::pos());
+	event->accept();
+}
+
+void SectionsView::click_debug() {
+	qDebug() << ui.SectionsTable->currentItem();
+}
+
+void SectionsView::copy_item() {
+	qDebug() << selectedItem;
+	QString content = selectedItem->text(); // 从选中的item获得字符串
+	QClipboard* clipboard = QApplication::clipboard();
+	clipboard->setText(content);
+}
+
 
 void SectionsView::AddItemsOneRow(int rowNo, QString name, DWORD virtualAddr, DWORD virtualSize, DWORD rawSize, int sizeDelta, DWORD characteristics) {
 	QTableWidget* table = ui.SectionsTable;
--- SectionsView.h
+++ SectionsView.h
@@ -2,6 +2,11 @@
 #include "PEImage.h"
 #include <qwidget.h>
 #include "ui_SectionsView.h"
+#include <qmenu.h>
+#include <qevent.h>
+#include <qclipboard.h>
+#include <qpoint.h>
+#include <qdebug.h>
 
 
 class SectionsView : public QWidget
@@ -17,10 +22,19 @@ public:
 
 private:
 	Ui::SectionsViewClass ui;
+	// TODO: 想想怎么把有menu功能的表格控件抽象出来
+	QMenu* menu = NULL;
+	QAction* action_copy;
+	QTableWidgetItem* selectedItem = NULL;
+	//void setup_menu();
+	//void show_menu(const QPoint pos);
+	void contextMenuEvent(QContextMenuEvent* event);
 
 	void setupTableView();
 	void AddItemsOneRow(int rowNo, QString name, DWORD virtualAddr, DWORD virtualSize, DWORD rawSize, int sizeDelta, DWORD characteristics);
 
 private slots:
+	void copy_item();
 	void closeView();
+	void click_debug();
 };
