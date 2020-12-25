f7a716f206c688808de46aed266bf8e305b0afcb
彭嘉欣 <pjx206@163.com>
2020-12-25 20:05:58 +0800

    能通过LineEdit打开文件

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -6,6 +6,9 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setAcceptDrops(true);
+
+    connect(ui->FilePathEdit, SIGNAL(editingFinished()), this, SLOT(setConfirmBtnEnabled())); // 手动输入完后，确认按钮能正确启用
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +16,59 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::dragEnterEvent(QDragEnterEvent* event)
+{
+    if (event->mimeData()->hasFormat("text/uri-list")) {
+        event->acceptProposedAction();
+    }
+}
+
+void MainWindow::dropEvent(QDropEvent* event)
+{
+    QList<QUrl> urls = event->mimeData()->urls();
+    if (urls.isEmpty())
+        return;
+    if (urls.count() > 1) {
+        QMessageBox::information(this, "Invalid Operation", "One file at a time!", QMessageBox::Ok);
+        return;
+    }
+
+    QUrl url = urls.first();
+    QString filepath = url.toLocalFile();   // 去掉url前面的"file://"
+    QLineEdit *le = ui->FilePathEdit;
+    qDebug() << filepath;
+
+    le->setText(filepath);
+    //ui->FilePathEdit->setDisabled(true);    // 设置禁止再编辑
+
+    emit(le->editingFinished());    // 避免还需要按回车才能启用确定按钮
+}
+
+void MainWindow::setConfirmBtnEnabled()
+{
+    ui->ConfirmFileBtn->setEnabled(true);
+}
+
+
+void MainWindow::openFileByLineEditPath() {
+    qDebug() << "clicked confirm button";
+    
+    QString filepath = ui->FilePathEdit->text();
+    qDebug() << "File path: " << filepath;
+    
+    openFileWithPath(filepath);
+}
+
+
+void MainWindow::openFileWithPath(QString filepath) {
+    QFile* pefile = new QFile(filepath);
+    if (pefile->exists() == false) {
+        QMessageBox::information(this, "File Error", "File not exists", QMessageBox::Ok);
+        return;
+    }
+
+    if (!pefile->open(QIODevice::ReadOnly)) {
+        QMessageBox::information(this, "File Error", "Cannot open file", QMessageBox::Ok);
+        return;
+    }
+}
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -2,20 +2,47 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <qdrag.h>
+#include <qevent.h>
+#include <qpoint.h>
+#include <qrect.h>
+#include <qfile.h>
+#include <qiodevice.h>
+#include <qmimedata.h>
+#include <qmessagebox.h>
+#include <qlineedit.h>
+#include <qdebug.h>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+
+signals:
+    void lineEditFilePathReady();
 
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+protected:
+    void dragEnterEvent(QDragEnterEvent* event);
+    void dropEvent(QDropEvent* event);
+
+private slots:
+    void openFileByLineEditPath();
+    void setConfirmBtnEnabled();
+
+private:
+    void openFileWithPath(QString path);
+
 private:
     Ui::MainWindow *ui;
 };
 #endif // MAINWINDOW_H
+
+
